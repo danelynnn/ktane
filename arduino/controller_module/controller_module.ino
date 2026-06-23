53b16ea676a71b86d8d38a0c21d98974a7cc0434
danelynnn <danelynnn@gmail.com>
2026-06-22 23:52:21 -0600

    COMMUNICATION MADE FLEXIBLE (modules can now join at any order with no desyncing)

--- arduino/controller_module/controller_module.ino
+++ arduino/controller_module/controller_module.ino
@@ -7,12 +7,23 @@
 #define I2C_ADDRESS 8
 #include <Wire.h>
 
-enum RequestType {
-  ACTIVATE, SERIALODD, SERIALVOWEL, BATTERY, TIMER, STRIKES
+enum OutputType
+{
+  ACTIVATE,
+  SERIALODD,
+  SERIALVOWEL,
+  BATTERY,
+  TIMER,
+  STRIKES,
+  RESET
 };
 
-enum Phase {
-  SETUP, PENDING, ACTIVE, SOLVED
+enum Phase
+{
+  SETUP,
+  READY,
+  ACTIVE,
+  SOLVED
 };
 
 char serial[] = "DL5QF3";
@@ -20,85 +31,90 @@ int batteries = 3;
 int strikes = 0;
 
 int numModules = 0;
-int modules[12];
-int phases[12];
+int addresses[12];
+int states[12];
 
 bool bombStatus = false;
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Wire.begin(I2C_ADDRESS);
 
+  Wire.begin(I2C_ADDRESS);
   Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
 
   // scan addresses for connected modules
-  for (int i=0; i<56; i++) {
+  for (int i = 1; i <= 56; i++)
+  {
     Wire.beginTransmission(I2C_ADDRESS + i);
-    
-    if (Wire.endTransmission() == 0) {
-      modules[numModules] = i;
-      numModules++;
+    Wire.write(RESET);
+    if (Wire.endTransmission() == 0)
+    {
+      Serial.println("done sending");
+      // request state from module
+      Wire.requestFrom(I2C_ADDRESS + i, 2);
+      receiveData(2);
+
+      // send starting variables to module
+      Wire.beginTransmission(I2C_ADDRESS + i);
+      Wire.write(SERIALODD);
+      Wire.write(createPacket(SERIALODD));
+      Wire.endTransmission();
     }
   }
 
-  // send starting variables to all connected modules
+  printState();
+}
+
+void tryStart()
+{
   bool ready = true;
-  for (int i=0; i<numModules; i++) {
-    Serial.print("pinging ");
-    Serial.println(I2C_ADDRESS + modules[i]);
 
-    Wire.beginTransmission(I2C_ADDRESS + modules[i]);
-    Wire.write(SERIALODD);
-    Wire.write(createPacket(SERIALODD));
-    Serial.println(Wire.endTransmission() == 0 ? "success" : "error");
-
-    // request status from module
-    Wire.requestFrom(I2C_ADDRESS + modules[i], 1);
-    while (Wire.available()) {
-      Phase phase = Wire.read();
-      phases[i] = phase;
-
-      if (phase != PENDING) {
-        ready = false;
-      }
-    }
-  }
+  for (int i = 0; i < numModules; i++)
+    if (states[i] != READY)
+      ready = false;
 
   // if ready, activate all modules
-  if (ready) {
-    for (int i=0; i<numModules; i++) {
+  if (ready)
+  {
+    for (int i = 0; i < numModules; i++)
+    {
       Serial.print("activating ");
-      Serial.println(I2C_ADDRESS + modules[i]);
+      Serial.println(I2C_ADDRESS + addresses[i]);
 
-      Wire.beginTransmission(I2C_ADDRESS + modules[i]);
-      Wire.write(ACTIVATE);
+      Wire.beginTransmission(I2C_ADDRESS + addresses[i]);
+      Wire.write(RESET);
       Wire.endTransmission();
     }
   }
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  
+
   // Wire.requestFrom(4, 16);
   // while (Wire.available()) { // peripheral may send less than requested
   //   char c = Wire.read(); // receive a byte as character
   //   Serial.print(c);         // print the character
   // }
-  printState();
   delay(1000);
 }
 
-byte createPacket(RequestType type) {
+byte createPacket(OutputType type)
+{
   byte data;
 
-  switch (type) {
+  switch (type)
+  {
   case SERIALODD:
     data = isSerialOdd();
     break;
   case SERIALVOWEL:
     data = hasSerialVowel();
+    break;
   case BATTERY:
     data = batteries ? batteries < 4 : 4;
     break;
@@ -115,48 +131,80 @@ byte createPacket(RequestType type) {
   return data;
 }
 
-void receiveData(int bytes) {
-  if (bytes == 2) {
-    int module = Wire.read();
-    for (int i=0; i<numModules; i++) {
-      if (module == modules[i]) {
+void receiveData(int bytes)
+{
+  if (bytes == 2)
+  {
+    int address = Wire.read();
+    int module = -1;
+    for (int i = 0; i < numModules; i++)
+    {
+      if (address == addresses[i])
+      {
         module = i;
         break;
       }
     }
 
+    if (module == -1)
+    {
+      addresses[numModules] = address;
+      module = numModules;
+      numModules++;
+    }
+
     int data = Wire.read();
     Serial.print("signal from ");
-    Serial.println(module);
-    
-    if (data == 255) {
+    Serial.print(address);
+    Serial.print(" data ");
+    Serial.println(data);
+
+    if (data == 255)
+    {
       strikes++;
-    } else if (data == 69) {
-      phases[module] = SOLVED;
+    }
+    else
+    {
+      states[module] = data;
     }
   }
 
   bool done = true;
-  for (int i=0; i<numModules; i++) {
-    if (phases[i] != SOLVED) {
+  for (int i = 0; i < numModules; i++)
+  {
+    if (states[i] != SOLVED)
+    {
       done = false;
     }
   }
-  if (done) {
+  if (done)
+  {
     bombStatus = true;
   }
+
+  printState();
 }
 
-void printArray(int arr[], int len) {
-  for (int i=0; i<len; i++) {
+void sendData()
+{
+  // Serial.println("sending");
+  Wire.write(SERIALODD);
+  Wire.write(createPacket(SERIALODD));
+}
+
+void printArray(int arr[], int len)
+{
+  for (int i = 0; i < len; i++)
+  {
     Serial.print(arr[i]);
     Serial.print(" ");
   }
 }
 
-void printState() {
-  Serial.print("module phases: ");
-  printArray(phases, numModules);
+void printState()
+{
+  Serial.print("module states: ");
+  printArray(states, numModules);
   Serial.print(", strikes: ");
   Serial.print(strikes);
   Serial.print(", remaining time: ");
@@ -164,12 +212,15 @@ void printState() {
   Serial.println(bombStatus ? "SOLVED" : "pending");
 }
 
-bool isSerialOdd() {
+bool isSerialOdd()
+{
   return (serial[5] - '0') % 2 == 1;
 }
 
-bool hasSerialVowel() {
-  for (int i=0; i<6; i++) {
+bool hasSerialVowel()
+{
+  for (int i = 0; i < 6; i++)
+  {
     if (serial[i] == 'A' || serial[i] == 'E' || serial[i] == 'I' || serial[i] == 'O' || serial[i] == 'U')
       return true;
   }
--- arduino/wires_module/wires_module.ino
+++ arduino/wires_module/wires_module.ino
@@ -1,8 +1,10 @@
+#define I2C_ADDRESS 8
 #define WIRE_SUCCESS 2
-#define WIRE_ONE 3  // Read pin
+#define WIRE_ONE 3 // Read pin
 #include <Wire.h>
 
-enum Color {
+enum Color
+{
   NONE,
   BLUE,
   RED,
@@ -12,224 +14,340 @@ enum Color {
   BLACK
 };
 
-enum RequestType {
-  ACTIVATE, SERIALODD, SERIALVOWEL, BATTERY, TIMER, STRIKES
+enum InputType
+{
+  ACTIVATE,
+  SERIALODD,
+  SERIALVOWEL,
+  BATTERY,
+  TIMER,
+  STRIKES,
+  RESET
 };
 
-enum Phase {
-  SETUP, PENDING, ACTIVE, SOLVED
+enum Phase
+{
+  SETUP,
+  READY,
+  ACTIVE,
+  SOLVED
 };
 
 Phase phase = SETUP;
-int state[] = {-1, -1, -1, -1, -1, -1};
+int states[] = {-1, -1, -1, -1, -1, -1};
 int answer = -1;
 
 // hardcoding things, this will eventually be randomised/sent from elsewhere
 Color colors[] = {RED, NONE, RED, YELLOW, BLUE, NONE};
-bool isSerialOdd = true;
+bool isSerialOdd = false;
 
-int countWires(Color color) {
+int countWires(Color color)
+{
   int count = 0;
-  for (int i=0; i<6; i++) {
-    if (colors[i] == color) {
+  for (int i = 0; i < 6; i++)
+  {
+    if (colors[i] == color)
+    {
       count++;
     }
   }
   return count;
 }
 
-void printArray(int arr[], int len) {
-  for (int i=0; i<len; i++) {
+void printArray(int arr[], int len)
+{
+  for (int i = 0; i < len; i++)
+  {
     Serial.print(arr[i]);
     Serial.print(" ");
   }
 }
 
-int handleRules(Color colors[]) {
+int handleRules(Color colors[])
+{
   int connectedWires[6]; // list of connected *indices*
   int numWires = 0;
 
-  for (int i=0; i<6; i++) {
-    if (colors[i] != NONE) {
+  for (int i = 0; i < 6; i++)
+  {
+    if (colors[i] != NONE)
+    {
       connectedWires[numWires++] = i;
     }
   }
 
-  switch(numWires) {
-    case 3:
-      if (countWires(RED) == 0) {
-        return connectedWires[1];
-      } else if (colors[connectedWires[numWires-1]] == WHITE) {
-        return connectedWires[numWires-1];
-      } else if (countWires(BLUE) > 1) {
-        // last blue wire
-        for (int i=numWires-1; i >= 0; i--) {
-          if (colors[connectedWires[i]] == BLUE) {
-            return connectedWires[i];
-          }
+  switch (numWires)
+  {
+  case 3:
+    if (countWires(RED) == 0)
+    {
+      return connectedWires[1];
+    }
+    else if (colors[connectedWires[numWires - 1]] == WHITE)
+    {
+      return connectedWires[numWires - 1];
+    }
+    else if (countWires(BLUE) > 1)
+    {
+      // last blue wire
+      for (int i = numWires - 1; i >= 0; i--)
+      {
+        if (colors[connectedWires[i]] == BLUE)
+        {
+          return connectedWires[i];
         }
-      } else {
-        return connectedWires[numWires-1];
-      } 
-      break;
-    case 4:
-      if (countWires(RED) > 1 && isSerialOdd) {
-        // last red wire
-        for (int i=numWires-1; i >= 0; i--) {
-          if (colors[connectedWires[i]] == RED) {
-            return connectedWires[i];
-          }
+      }
+    }
+    else
+    {
+      return connectedWires[numWires - 1];
+    }
+    break;
+  case 4:
+    if (countWires(RED) > 1 && isSerialOdd)
+    {
+      // last red wire
+      for (int i = numWires - 1; i >= 0; i--)
+      {
+        if (colors[connectedWires[i]] == RED)
+        {
+          return connectedWires[i];
         }
-      } else if (colors[connectedWires[numWires-1]] == YELLOW && countWires(RED) == 0) {
-        return connectedWires[0];
-      } else if (countWires(BLUE) == 1) {
-        return connectedWires[0];
-      } else if (countWires(YELLOW) > 1) {
-        return connectedWires[numWires-1];
-      } else {
-        return connectedWires[1];
       }
-      break;
-    case 5:
-      if (colors[connectedWires[numWires-1]] == BLACK && isSerialOdd) {
-        return connectedWires[3];
-      } else if (countWires(RED) == 1 && countWires(YELLOW) > 1) {
-        return connectedWires[0];
-      } else if (countWires(BLACK) == 0) {
-        return connectedWires[1];
-      } else {
-        return connectedWires[0];
-      }
-      break;
-    case 6:
-      if (countWires(YELLOW) == 0 && isSerialOdd) {
-        return connectedWires[2];
-      } else if (countWires(YELLOW) == 1 && countWires[WHITE] > 1) {
-        return connectedWires[3];
-      } else if (countWires(RED) == 0) {
-        return connectedWires[numWires - 1];
-      } else {
-        return connectedWires[3];
-      }
-      break;
+    }
+    else if (colors[connectedWires[numWires - 1]] == YELLOW && countWires(RED) == 0)
+    {
+      return connectedWires[0];
+    }
+    else if (countWires(BLUE) == 1)
+    {
+      return connectedWires[0];
+    }
+    else if (countWires(YELLOW) > 1)
+    {
+      return connectedWires[numWires - 1];
+    }
+    else
+    {
+      return connectedWires[1];
+    }
+    break;
+  case 5:
+    if (colors[connectedWires[numWires - 1]] == BLACK && isSerialOdd)
+    {
+      return connectedWires[3];
+    }
+    else if (countWires(RED) == 1 && countWires(YELLOW) > 1)
+    {
+      return connectedWires[0];
+    }
+    else if (countWires(BLACK) == 0)
+    {
+      return connectedWires[1];
+    }
+    else
+    {
+      return connectedWires[0];
+    }
+    break;
+  case 6:
+    if (countWires(YELLOW) == 0 && isSerialOdd)
+    {
+      return connectedWires[2];
+    }
+    else if (countWires(YELLOW) == 1 && countWires[WHITE] > 1)
+    {
+      return connectedWires[3];
+    }
+    else if (countWires(RED) == 0)
+    {
+      return connectedWires[numWires - 1];
+    }
+    else
+    {
+      return connectedWires[3];
+    }
+    break;
   }
 }
 
-void printState() {
+void printState()
+{
   Serial.print("we are in phase ");
   Serial.print(phase);
   Serial.print(", current state is ");
-  printArray(state, 6);
+  printArray(states, 6);
 
-  if (phase == ACTIVE) {
+  if (phase == ACTIVE)
+  {
     Serial.print(", correct answer is ");
     Serial.print(answer);
   }
-  
+
   Serial.println();
 }
 
-void onChange(int pin, int status) {
-  Serial.print("pin ");
-  Serial.print(pin);
-  Serial.print(" has been set to ");
-  Serial.print(status);
-  Serial.println();
+// returns whether should report to controller
+bool update(int pin)
+{
+  bool shouldReport = false;
 
-  if (phase == SETUP) {
+  if (phase == SETUP)
+  {
     bool ready = true;
-    for (int i=0; i<6; i++) {
-      if ((colors[i] != NONE && state[i] == 1) || (colors[i] == NONE && state[i] == 0)) {
-      } else {
+    for (int i = 0; i < 6; i++)
+    {
+      if ((colors[i] != NONE && states[i] == 1) || (colors[i] == NONE && states[i] == 0))
+      {
+      }
+      else
+      {
         ready = false;
       }
     }
 
-    if (ready) {
-      phase = 1;
-      answer = handleRules(colors);
+    if (ready)
+    {
+      phase = READY;
+      shouldReport = true;
     }
-  } else if (phase == ACTIVE) {
-    if (pin == answer) {
+  }
+  else if (phase == READY || phase == ACTIVE)
+  {
+    if (pin == answer)
+    {
       phase = SOLVED;
-      digitalWrite(WIRE_SUCCESS, true);
-
-      Wire.beginTransmission(8);
-      Wire.write(1);
-      Wire.write(69);
-      Wire.endTransmission();
-    } else {
+      shouldReport = true;
+    }
+    else
+    {
       Serial.println("mistake");
 
       Wire.beginTransmission(8);
-      Wire.write(1);
+      Wire.write(9);
       Wire.write(-1);
       Wire.endTransmission();
     }
   }
 
+  if (phase == SOLVED)
+  {
+    digitalWrite(WIRE_SUCCESS, true);
+  }
+  else
+  {
+    digitalWrite(WIRE_SUCCESS, false);
+  }
   printState();
+
+  return shouldReport;
 }
 
-void setup() {
+void setPin(int pin, int state)
+{
+  states[pin] = state;
+
+  Serial.print("pin ");
+  Serial.print(pin);
+  Serial.print(" has been set to ");
+  Serial.print(state);
+  Serial.println();
+
+  if (update(pin))
+  {
+    Wire.beginTransmission(8);
+    sendData();
+    Wire.endTransmission();
+  }
+}
+
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
 
   Wire.begin(9);
   Wire.onReceive(receiveData);
-  Wire.onRequest(requestData);
+  Wire.onRequest(sendData);
 
   pinMode(WIRE_SUCCESS, OUTPUT);
   // set all pins as input
-  for (int i=0; i<6; i++)
-    pinMode(WIRE_ONE+i, INPUT);
-  
+  for (int i = 0; i < 6; i++)
+    pinMode(WIRE_ONE + i, INPUT);
+
+  Serial.println("setup");
+
+  // scan for controller
+  Wire.beginTransmission(I2C_ADDRESS);
+  sendData(); // send state to controller
+  if (Wire.endTransmission() == 0)
+  {
+    // request data from controller
+    Wire.requestFrom(I2C_ADDRESS, 2);
+    receiveData(2);
+  }
+
   Serial.println("setup complete, starting state");
   printState();
 }
 
 int readout[] = {0, 0, 0, 0, 0, 0};
 int debounce[] = {0, 0, 0, 0, 0, 0};
-void loop() {
-  for (int i=0; i<6; i++)
-    readout[i] = digitalRead(WIRE_ONE+i);
-  
-  for (int i=0; i<6; i++) {
-    if (readout[i] != state[i]) {
-      // state[i] = readout[i];
-      // onChange(i, state[i]);
+void loop()
+{
+  for (int i = 0; i < 6; i++)
+    readout[i] = digitalRead(WIRE_ONE + i);
 
+  for (int i = 0; i < 6; i++)
+  {
+    if (readout[i] != states[i])
+    {
       debounce[i]++;
-      if (debounce[i] == 10000) {
-        state[i] = readout[i];
-        onChange(i, state[i]);
+      if (debounce[i] == 10000)
+      {
+        setPin(i, readout[i]);
 
         debounce[i] = 0;
       }
-    } else {
+    }
+    else
+    {
       debounce[i] = 0;
     }
   }
 }
 
-void receiveData(int bytes) {
-  RequestType signal = Wire.read();
-  Serial.print("received data, type ");
-  Serial.println(signal);
+void receiveData(int bytes)
+{
+  InputType signal = Wire.read();
   int data = Wire.read();
 
-  switch (signal) {
+  Serial.print("received data, type ");
+  Serial.print(signal);
+  Serial.print(" data ");
+  Serial.println(data);
+
+  switch (signal)
+  {
   case ACTIVATE:
-    Serial.println("activate received");
     phase = ACTIVE;
+    answer = handleRules(colors);
     printState();
+    break;
   case SERIALODD:
     isSerialOdd = data;
+    answer = handleRules(colors);
+    break;
+  case RESET:
+    phase = SETUP;
+    update(-1);
+    break;
   }
 }
 
-void requestData() {
-  Serial.print("sending status ");
-  Serial.println(phase);
+void sendData()
+{
+  // Serial.println("sending");
+  Wire.write(9);
   Wire.write(phase);
 }
